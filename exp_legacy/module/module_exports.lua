65145b5d34c2c02c5effcf021d22db22c326d84c
Cooldude2606 <25043174+Cooldude2606@users.noreply.github.com>
2024-09-23 15:55:28 +0100

    Move files to exp_legacy

---
+++ exp_legacy/module/module_exports.lua
@@ -0,0 +1,3 @@
+--- Can contain anything you want to allow other plugins to have access to, this example exposes the control file
+-- Access the exports from other modules using require("modules/exp_legacy")
+return require("modules/exp_legacy/control")
