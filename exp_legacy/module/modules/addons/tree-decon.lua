b13bbcc0b500feabe76c530b9215aafc01023e8b
PHIDIAS <phidias@aperx.org>
2025-01-01 17:41:57 +0900

    Update tree-decon.lua

--- exp_legacy/module/modules/addons/tree-decon.lua
+++ exp_legacy/module/modules/addons/tree-decon.lua
@@ -117,12 +117,12 @@ Event.add(defines.events.on_entity_damaged, function(event)
     end
 
     local driver = event.cause.get_driver()
-    if not driver then return end
 
-    local allow = get_permission(driver.player.index)
-    if allow == "fast" and HasEnabledDecon:get(driver.player) then
-        event.entity.destroy()
-    else
-        event.entity.order_deconstruction(event.force, driver.player)
+    if driver and driver.player then
+        if get_permission(driver.player.index) == "fast" and HasEnabledDecon:get(driver.player) then
+            event.entity.destroy()
+        else
+            event.entity.order_deconstruction(event.force, driver.player)
+        end
     end
 end)
