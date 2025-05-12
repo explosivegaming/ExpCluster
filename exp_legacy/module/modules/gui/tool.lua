54eeb667749f3f06c2b3d107fd481bd990c35ea7
PHIDIAS <phidias@aperx.org>
2025-05-12 22:04:14 +0900

    Update tool.lua

--- exp_legacy/module/modules/gui/tool.lua
+++ exp_legacy/module/modules/gui/tool.lua
@@ -50,10 +50,11 @@ local tool_gui_arty_b = Gui.element("tool_gui_arty_b")
     ):on_click(function(def, player, element)
         if Selection.is_selecting(player, SelectionArtyArea) then
             Selection.stop(player)
+            player.print{ "exp-commands_waterfill.exit" }
 
         else
             Selection.start(player, SelectionArtyArea)
-            player.print{ "tool.entered-area-selection" }
+            player.print{ "exp-commands_waterfill.enter" }
         end
     end)
 
