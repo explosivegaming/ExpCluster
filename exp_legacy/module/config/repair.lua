c454b4058163771bd13b8dd0562ebf2ed0339d6c
PHIDIAS <phidias@aperx.org>
2026-06-26 14:20:31 +0900

    Update repair.lua

--- exp_legacy/module/config/repair.lua
+++ exp_legacy/module/config/repair.lua
@@ -2,7 +2,6 @@
 -- @config Repair
 
 return {
-    max_range = 50, --- @setting max_range the max range that can be used with the repair command
     allow_blueprint_repair = false, --- @setting allow_blueprint_repair when true will allow blueprints (things not destroyed by biters) to be build instantly using the repair command
     allow_ghost_revive = true, --- @setting allow_ghost_revive when true will allow ghosts (things destroyed by biters) to be build instantly using the repair command
     allow_heal_entities = true, --- @setting allow_heal_entities when true will heal entities to full health that are within range
